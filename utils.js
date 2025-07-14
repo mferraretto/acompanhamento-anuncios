428946c1836b893e215c588c72db6450538a5b05
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-14 14:03:31 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
 export const sanitize = (v) => (v == null ? '' : String(v));
+export const removeInvalid = (obj) => {
+  const out = {};
+  for (const [k, v] of Object.entries(obj)) {
+    if (v !== undefined && (typeof v !== 'number' || Number.isFinite(v))) {
+      out[k] = v;
+    }
+  }
+  return out;
+};
