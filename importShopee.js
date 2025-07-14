05dea3ee29bb24eacd01932f537a2373ea654ddf
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-14 14:17:23 -0300

    Update importShopee.js

--- importShopee.js
+++ importShopee.js
@@ -1,14 +1,6 @@
 // Parse and merge Shopee spreadsheets (basic, media and shipping)
-import { sanitize } from './utils.js';
-const removeInvalid = (obj) => {
-  const out = {};
-  for (const [k, v] of Object.entries(obj)) {
-    if (v !== undefined && (typeof v !== 'number' || Number.isFinite(v))) {
-      out[k] = v;
-    }
-  }
-  return out;
-};
+import { sanitize, removeInvalid } from './utils.js';
+
 document.getElementById('btnSalvarShopeePlanilhas').addEventListener('click', async () => {
   try {
     const input = document.getElementById('inputShopeePlanilhas');
