703853919dd5a7329d1c51a705718bae707c32d2
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-14 22:16:13 -0300

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDovLKo3djdRbs963vqKdbj-geRWyzMTrg",
+  authDomain: "acompanhamento-anuncios.firebaseapp.com",
+  projectId: "acompanhamento-anuncios",
+  storageBucket: "acompanhamento-anuncios.appspot.com",
+  messagingSenderId: "993424162956",
+  appId: "1:993424162956:web:61e1b0b40bd6c36cb9bfc4"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+window.db = db;
