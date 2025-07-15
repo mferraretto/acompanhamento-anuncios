0251b5455c338292013dcf5a587aab65d7dcc11b
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-14 22:00:59 -0300

    Update and rename firebase.config.example.js to firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,4 +1,5 @@
-window.firebaseConfig = {
+// firebase.config.js
+const firebaseConfig = {
   apiKey: "AIzaSyAJHISEca3wibw92Bw6UQCpQuyhtkjAoWE",
   authDomain: "acompanhamento-anuncios.firebaseapp.com",
   projectId: "acompanhamento-anuncios",
@@ -6,3 +7,6 @@ window.firebaseConfig = {
   messagingSenderId: "993424162956",
   appId: "1:993424162956:web:37bb9a39d8aee076cdfba9"
 };
+
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.firestore();
