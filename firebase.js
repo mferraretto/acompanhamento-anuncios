735f88acffa2d892fc4e7e36c6aaae7dd2ff6ae5
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-15 07:59:00 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,20 +2,18 @@ const firebaseConfig =
   typeof window !== 'undefined' && window.firebaseConfig
     ? window.firebaseConfig
     : {
-      apiKey: "AIzaSyAJHISEca3wibw92Bw6UQCpQuyhtkjAoWE",
-  authDomain: "acompanhamento-anuncios.firebaseapp.com",
-  projectId: "acompanhamento-anuncios",
-  storageBucket: "acompanhamento-anuncios.appspot.com",
-  messagingSenderId: "993424162956",
-  appId: "1:993424162956:web:37bb9a39d8aee076cdfba9"
-};
+        apiKey: "AIzaSyAJHISEca3wibw92Bw6UQCpQuyhtkjAoWE",
+        authDomain: "acompanhamento-anuncios.firebaseapp.com",
+        projectId: "acompanhamento-anuncios",
+        storageBucket: "acompanhamento-anuncios.appspot.com",
+        messagingSenderId: "993424162956",
+        appId: "1:993424162956:web:37bb9a39d8aee076cdfba9"
+      };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// Disponibiliza para códigos legados que esperam window.db
+// ✅ Define window.db apenas se estiver no navegador
 if (typeof window !== 'undefined') {
   window.db = db;
 }
-
-window.db = firebase.firestore();
