d1707ff1934c1e51a3ed6a1a23b03ed861fb3686
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-14 21:07:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
 const CACHE_NAME = 'app-cache-v1';
 const ASSETS = [
-  '/',
-  'index.html',
-  'style.css',
-  'app.js',
-  'tabs.js',
-  'firebase.config.js',
-  'firebase.js',
-  'importShopee.js',
-  'importPedidos.js',
-  'manifest.json',
-  'icon-192.png'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './tabs.js',
+  './firebase.js',
+  './importShopee.js',
+  './importPedidos.js',
+  './importDesempenho.js',
+  './manifest.json',
+  './icon-192.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-       caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -28,6 +28,7 @@ self.addEventListener('activate', event => {
     )
   );
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
