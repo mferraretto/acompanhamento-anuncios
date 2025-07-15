536c6b7812d6bbed5db52178cc60f3c2f976a669
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-14 21:06:00 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
-let firebaseConfig = typeof window !== 'undefined' && window.firebaseConfig ? window.firebaseConfig : {
- apiKey: "AIzaSyAJHISEca3wibw92Bw6UQCpQuyhtkjAoWE",
+// firebase.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJHISEca3wibw92Bw6UQCpQuyhtkjAoWE",
   authDomain: "acompanhamento-anuncios.firebaseapp.com",
   projectId: "acompanhamento-anuncios",
   storageBucket: "acompanhamento-anuncios.appspot.com",
   messagingSenderId: "993424162956",
   appId: "1:993424162956:web:37bb9a39d8aee076cdfba9"
 };
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-export { db };
+window.db = db; // Torna o 'db' acessível globalmente
