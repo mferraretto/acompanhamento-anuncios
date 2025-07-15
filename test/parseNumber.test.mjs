931f1591097b11c11f236ed58269709bde1db1cc
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-15 07:49:38 -0300

    Update parseNumber.test.mjs

--- test/parseNumber.test.mjs
+++ test/parseNumber.test.mjs
@@ -3,5 +3,7 @@ import { parseNumber } from '../utils.js';
 
 assert.strictEqual(parseNumber('1.234,56'), 1234.56);
 assert.strictEqual(parseNumber('1234.56'), 1234.56);
+assert.strictEqual(parseNumber('50%'), 50);
+assert.strictEqual(parseNumber('R$ 1.234,56'), 1234.56);
 
 console.log('parseNumber tests passed');
